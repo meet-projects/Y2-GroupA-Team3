00cf8ab6211e2ab85893dab617d96c61d4097ddb
leenar24-meet <139689053+leenar24-meet@users.noreply.github.com>
2023-07-31 12:08:19 +0300

    first edit by leen

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ import pyrebase
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+config = {
+  "apiKey": "AIzaSyCWJvL11O1Q983LXR218a51EGNZHUHOMps",
+  "authDomain": "groupprojectweb-5d2dd.firebaseapp.com",
+  "projectId": "groupprojectweb-5d2dd",
+  "storageBucket": "groupprojectweb-5d2dd.appspot.com",
+  "messagingSenderId": "521418284281",
+  "appId": "1:521418284281:web:20ae20233d2020f9b75628",
+  "measurementId": "G-5WB87QXG2D",
+  "databaseURL" : "https://groupprojectweb-5d2dd-default-rtdb.europe-west1.firebasedatabase.app/"
+};
 
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db= firebase.database()
 #Code goes below here
 
 
